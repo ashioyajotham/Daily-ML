0ab9c99ba05958bc5fcef27b41eaca90fb89232e
ashioyajotham <victorashioya960@gmail.com>
2023-10-02 14:40:52 +0300

    django demo worked

---
+++ Django Model Deployment/demo/basicConcepts/views.py
@@ -0,0 +1,16 @@
+# The basic concept of django is called - MVT ie Model, View, Template
+# Model: It is the database layer. It contains the data. It is the data access layer.
+# View: It is the business logic layer. It contains the logic of the application.
+# Template: It is the presentation layer. It contains the presentation logic of the application.
+
+# The other concept is MVC ie Model, View, Controller
+# Model: It is the database layer. It contains the data. It is the data access layer.
+# View: It is the presentation layer. It contains the presentation logic of the application.
+# Controller: It is the business logic layer. It contains the logic of the application.
+
+# Django follows MVT architecture. It is a variation of MVC architecture.
+# In MVT, the controller is replaced by the template.
+
+from django.http import HttpResponse
+def Welcome(request):
+    return HttpResponse("<h1>Welcome to Django</h1>")
--- Django Model Deployment/djangorest/readme.md
+++
@@ -1,3 +0,0 @@
-# Django REST Application to Deploy Machine Learning Models
-* `Django REST` framework is a powerful and flexible toolkit for building Web APIs which can be used to deploy machine learning models.
-* `Django REST` framework is a `Python`-based framework for building web APIs. It is a collection of tools and resources that help in building `RESTful` APIs.
